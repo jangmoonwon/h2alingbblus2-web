248c9cb65e80e561ed2a04dff60332724e1491b9
MOON <muneon96@naver.com>
2021-08-18 22:49:39 +0900

    Header 링크 설정 및 기본구조 설정

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import { Container } from './styles';
+import React from "react";
+import { Container, LinkWrapper, MainLink, TextLink } from "./styles";
+import paths from "../../routes/paths.json";
 
 export default function Header() {
-    return (
-        <Container>
-            <h2>header</h2>
-        </Container>
-    );
+  return (
+    <Container>
+      <h2>header</h2>
+      <MainLink to={paths.main}>(Main)</MainLink>
+      <LinkWrapper>
+        <TextLink to={paths.about}>About</TextLink>
+        <TextLink to={paths.worship}>Worship</TextLink>
+        <TextLink to={paths.photo}>Photo</TextLink>
+      </LinkWrapper>
+    </Container>
+  );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,12 +1,39 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Container = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
+  justify-content: space-between;
   width: 100%;
   height: 170px;
   border-bottom: 1px solid #e5e5e5;
 `;
 
-export { Container };
+const MainLink = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 30px;
+  text-decoration: none;
+`;
+
+const LinkWrapper = styled.div`
+  display: flex;
+  gap: 30px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin: 20px;
+`;
+
+const TextLink = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  text-decoration: none;
+`;
+
+export { Container, TextLink, MainLink, LinkWrapper };
