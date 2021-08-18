375718a9987fb2c4baf5a0e357765b6f736b29a0
MOON <muneon96@naver.com>
2021-08-18 22:47:15 +0900

    Page 생성

---
+++ src/containers/PhotoPage/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PhotoPage() {
+    return (
+        <div>
+            <h1>Photo Pages.</h1>
+        </div>
+    );
+}
+
+export default PhotoPage;
---
+++ src/containers/WorshipPage/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function WorshipPage() {
+    return (
+        <div>
+            <h1>Worship Pages.</h1>
+        </div>
+    );
+}
+
+export default WorshipPage;
