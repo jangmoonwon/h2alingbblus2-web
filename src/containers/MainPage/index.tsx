dad7c75fe05f0594f2b50957b358cf23bc13bc81
MOON <muneon96@naver.com>
2021-08-20 19:58:09 +0900

    images 파일 추가, header 메인로고 이미지 삽입, footer 링크 추가

--- src/containers/MainPage/index.tsx
+++ src/containers/MainPage/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { BackgroundImg } from "./styles";
 
 function MainPage() {
-    return (
-        <div>
-            <h1>Main Pages.</h1>
-        </div>
-    );
+  return (
+    <div>
+      <BackgroundImg src="/images/mainBg.jpg" alt="main img"></BackgroundImg>
+      <h1>Main Pages.</h1>
+    </div>
+  );
 }
 
 export default MainPage;
---
+++ src/containers/MainPage/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const BackgroundImg = styled.img`
+  width: 100vw;
+  height: 100vh;
+`;
+
+export { BackgroundImg };
