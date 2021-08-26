5046e484edd53640cfc63a3a152f8bfe54683c27
MOON <muneon96@naver.com>
2021-08-26 18:03:58 +0900

    메인페이지 배경이미지 스타일링 수정

--- src/containers/MainPage/index.tsx
+++ src/containers/MainPage/index.tsx
@@ -4,9 +4,9 @@ import { Container, Wrapper } from "./styles";
 function MainPage() {
   return (
     <Container>
-      <Wrapper>Main Pages_1</Wrapper>
-      <Wrapper style={{backgroundColor: 'red'}}>Main Pages_2</Wrapper>
-      <Wrapper style={{backgroundColor: 'blue'}}>Main Pages_3</Wrapper>
+      <Wrapper style={{color: "#fff", fontSize: 100 ,fontWeight: 600}}>Main Pages_1</Wrapper>
+      <Wrapper style={{backgroundColor: 'ivory'}}>Main Pages_2</Wrapper>
+      <Wrapper style={{backgroundColor: 'white'}}>Main Pages_3</Wrapper>
     </Container>
   );
 }
