af34c45a5135facc24677ac568b5cc170789ebac
MOON <muneon96@naver.com>
2021-08-27 17:25:19 +0900

    폰트 추가, 글로벌스타일 폰트 저장

--- src/globals/styles.js
+++ src/globals/styles.js
@@ -1,7 +1,22 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+    @font-face {
+        font-family: 'GowunBatang-Regular';
+        src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2108@1.1/GowunBatang-Regular.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+
+    @font-face {
+        font-family: 'Cafe24Oneprettynight';
+        src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_twelve@1.1/Cafe24Oneprettynight.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+}
+}
+
     body {
         margin: 0 auto;
+        font-family: "Cafe24Oneprettynight";
     }
 `;
