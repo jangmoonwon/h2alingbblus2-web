6f4c572c11148e01f78717c140b97482065f218f
MOON <muneon96@naver.com>
2021-10-13 00:45:21 +0900

    카카오맵 api 생성

--- src/containers/WorshipPage/index.tsx
+++ src/containers/WorshipPage/index.tsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Wrapper } from "./styles";
+// import  { Map, MapMarker } from "react-kakao-maps-sdk";
 
-// 37.74528874052894, 126.48116906875377 //
+// 강화중앙교회 경도&위도 37.74528874052894, 126.48116906875377 //
+
 declare global {
   interface Window {
     kakao: any;
@@ -12,22 +14,20 @@ const { kakao } = window;
 
 function Worship() {
   const [error, setError] = useState(false);
+
   useEffect(() => {
-    // create map
     try {
       const container = document.getElementById("kakaomap");
       const options = {
-        center: new kakao.maps.LatLng(37.74528874052894, 126.48116906875377),
+        center: new kakao.maps.LatLng(37.745054, 126.481176),
         level: 3,
       };
-
       const map = new kakao.maps.Map(container, options);
 
-      // create address-coordinates change obj
       const geocoder = new kakao.maps.services.Geocoder();
       // research coordinates from address
       geocoder.addressSearch(
-        "인천 강화군 강화읍 충렬사로 138",
+        "인천광역시 강화군 강화읍 청하동길 36 강화중앙교회",
         function (result: any, status: any) {
           if (status === kakao.maps.services.Status.OK) {
             var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
@@ -41,7 +41,7 @@ function Worship() {
             // custom location info
             var infowindow = new kakao.maps.InfoWindow({
               content:
-                '<div style="width:150px;text-align:center;padding:6px 0;color:#000;">Here!</div>',
+                '<div style="width:150px;text-align:center;padding: 6px;color:#000;font-size:15px">강화중앙교회 입구</div>',
             });
             infowindow.open(map, marker);
 
@@ -59,19 +59,14 @@ function Worship() {
   return (
     <Container>
       <Wrapper>
-        <div
-          id="kakaomap"
-          style={{
-            width: "500px",
-            height: "500px",
-          }}
-        >
+      <div id="kakaomap" style={{ width: "360px", height: "360px" }}>
           {error ? (
             <p style={{ textAlign: "center", color: "darkgrey" }}>
               일시적인 오류로 인해 지도 확인이 불가능합니다
             </p>
           ) : undefined}
         </div>
+
       </Wrapper>
     </Container>
   );
