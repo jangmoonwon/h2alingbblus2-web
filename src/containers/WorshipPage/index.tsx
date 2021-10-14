2aa8a3e8a0693bc24364d7afdfcd6a60e4765cf3
MOON <muneon96@naver.com>
2021-10-14 23:04:47 +0900

    예배안내 페이지 틀 잡기

--- src/containers/WorshipPage/index.tsx
+++ src/containers/WorshipPage/index.tsx
@@ -1,8 +1,15 @@
-import React, { useState, useEffect } from "react";
-import { Container, Wrapper } from "./styles";
-// import  { Map, MapMarker } from "react-kakao-maps-sdk";
-
-// 강화중앙교회 경도&위도 37.74528874052894, 126.48116906875377 //
+import React, { useState, useEffect, useRef } from "react";
+import {
+  Container,
+  Wrapper,
+  Map,
+  ErrorText,
+  Box,
+  Box2,
+  ScrollBtn,
+  Text,
+  Hr,
+} from "./styles";
 
 declare global {
   interface Window {
@@ -15,37 +22,37 @@ const { kakao } = window;
 function Worship() {
   const [error, setError] = useState(false);
 
+  const TopRef = useRef<any>();
+  const scrollToTop = () => {
+    TopRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
   useEffect(() => {
     try {
       const container = document.getElementById("kakaomap");
       const options = {
         center: new kakao.maps.LatLng(37.745054, 126.481176),
         level: 3,
+        draggable: false,
       };
       const map = new kakao.maps.Map(container, options);
 
       const geocoder = new kakao.maps.services.Geocoder();
-      // research coordinates from address
+
       geocoder.addressSearch(
         "인천광역시 강화군 강화읍 청하동길 36 강화중앙교회",
         function (result: any, status: any) {
           if (status === kakao.maps.services.Status.OK) {
             var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
-
-            // indication marker
             var marker = new kakao.maps.Marker({
               map: map,
               position: coords,
             });
-
-            // custom location info
             var infowindow = new kakao.maps.InfoWindow({
               content:
                 '<div style="width:150px;text-align:center;padding: 6px;color:#000;font-size:15px">강화중앙교회 입구</div>',
             });
             infowindow.open(map, marker);
-
-            // move to center
             map.setCenter(coords);
           }
         }
@@ -59,15 +66,22 @@ function Worship() {
   return (
     <Container>
       <Wrapper>
-      <div id="kakaomap" style={{ width: "360px", height: "360px" }}>
-          {error ? (
-            <p style={{ textAlign: "center", color: "darkgrey" }}>
-              일시적인 오류로 인해 지도 확인이 불가능합니다
-            </p>
-          ) : undefined}
-        </div>
-
+        <Text>WORSHIP</Text>
+        <Hr />
+        <ScrollBtn onClick={scrollToTop}>지도보기</ScrollBtn>
       </Wrapper>
+      <Box>
+        <h1>1</h1>
+      </Box>
+      <Box2 ref={TopRef}>
+      <Map id="kakaomap">
+          {error ? (
+            <ErrorText>
+              일시적인 오류로 인해 지도 확인이 불가능합니다.
+            </ErrorText>
+          ) : undefined}
+        </Map>
+      </Box2>
     </Container>
   );
 }
--- src/containers/WorshipPage/styles.ts
+++ src/containers/WorshipPage/styles.ts
@@ -3,9 +3,13 @@ import styled from "styled-components";
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
+  overflow: auto;
+  scroll-snap-type: y mandatory;
 `;
 
 const Wrapper = styled.div`
+  position: absolute;
+  scroll-snap-align: center;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -15,4 +19,66 @@ const Wrapper = styled.div`
   font-weight: 600;
 `;
 
-export { Container, Wrapper };
+const Text = styled.text`
+text-align: center;
+font-family: "NanumSquareExtraBold";
+font-weight: 900;
+font-size: 50px;
+`;
+
+const Hr = styled.hr`
+width: 47px;
+height: 3px;
+background-color: black;
+border: transparent;
+`;
+
+const Box = styled.div`
+width: 100vw;
+height: 100vh;
+position: relative;
+z-index: 3;
+background-color: blue;
+top: 100%;
+overflow: auto;
+`;
+
+const Box2 = styled.div`
+width: 100vw;
+height: 100vh;
+position: relative;
+z-index: 5;
+background-color: black;
+opacity: 0.67;
+top: 100%;
+overflow: auto;
+`;
+
+const ErrorText = styled.text`
+  font-size: 50px;
+  text-align: center;
+  color: #adacac;
+`;
+
+const Map = styled.div`
+  width: 80vw;
+  height: 70vh;
+`;
+
+const ScrollBtn = styled.button`
+  position: absolute;
+  z-index: 10;
+  bottom: 5%;
+  width: 300px;
+  height: 45px;
+  background-color: transparent;
+  border: transparent;
+  font-size: 43px;
+  font-family: "Cafe24Oneprettynight";
+  font-weight: 300;
+  color: #003638;
+  cursor: pointer;
+`;
+
+
+export { Container, Wrapper, Map, ErrorText, Box, Box2, ScrollBtn, Text, Hr };
