b212b687e85b2d07115f980c95639a32a47452a5
MOON <muneon96@naver.com>
2021-10-27 21:44:04 +0900

    포토앨범(청년부 수련회) 페이지 생성

--- src/containers/PhotoPage/Album1.tsx
+++ src/containers/PhotoPage/Album1.tsx
@@ -1,19 +1,33 @@
 import React from "react";
 import styled from "styled-components";
 
-type ButtonProps = {
-  history: any;
-}
-
-function Album1({ history }: ButtonProps) {
+const Album1 = () => {
   return (
     <Container>
-      <HeaderTitle>앨범 제목</HeaderTitle>
-     <Wrapper />
-      {/* <Prevbutton onClick={() => history.goBack()}>Back</Prevbutton> */}
+      <HeaderTitle>청년부 수련회</HeaderTitle>
+      <ImageContainer>
+        <Wrapper>
+          <Images src="/images/mainImg.jpg" />
+          <Images src="/images/colorPhotoTree.jpg" />
+          <Images src="/images/album1/album1(1).jpg" />
+          <Images src="/images/album1/album1(2).jpg" />
+          <Images src="/images/album1/album1(3).jpg" />
+          <Images src="/images/album1/album1(4).jpg" />
+          <Images src="/images/album1/album1(5).jpg" />
+          <Images src="/images/album1/album1(6).jpg" />
+          <Images src="/images/album1/album1(7).jpg" />
+          <Images src="/images/album1/album1(8).jpg" />
+          <Images src="/images/album1/album1(9).jpg" />
+          <Images src="/images/album1/album1(10).jpg" />
+          <Images src="/images/album1/album1(11).jpg" />
+          <Images src="/images/album1/album1(12).jpg" />
+
+        </Wrapper>
+        <Prevbutton>BACK</Prevbutton>
+      </ImageContainer>
     </Container>
   );
-}
+};
 
 export default Album1;
 
@@ -23,20 +37,25 @@ const Container = styled.div`
   width: 100vw;
   height: 100vh;
   background-color: ivory;
+  gap: 20px;
+`;
+
+const ImageContainer = styled.div`
+  width: 100vw;
+  height: 80vh;
+  overflow: auto;
+  scroll-snap-type: y mandatory;
+  /* background-color: blueviolet; */
 `;
 
 const Wrapper = styled.div`
-  position: relative;
   scroll-snap-align: center;
+  width: 100vw;
   display: flex;
-  flex-direction: column;
+  flex-wrap: wrap;
   justify-content: center;
-  align-items: center;
-  height: 85vh;
-  font-size: 35px;
-  font-weight: 600;
-  background-color: #f5ebe9;
-  `;
+  gap: 20px 20px;
+`;
 
 const HeaderTitle = styled.div`
   display: flex;
@@ -47,18 +66,23 @@ const HeaderTitle = styled.div`
   font-size: 55px;
   font-family: "Cafe24Oneprettynight";
   font-weight: 900;
-  /* background-color: khaki; */
+
 `;
 
+const Images = styled.img`
+  width: 700px;
+  height: 500px;
+  object-fit: cover;
+`;
 
-
-// const Prevbutton = styled.button`
-// left: 45%;
-// bottom: 3%;
-// width: 120px;
-// height: 50px;
-// border: transparent;
-// border-radius: 23px;
-// background-color: turquoise;
-// font-size: 30px;
-// `;
+const Prevbutton = styled.button`
+position: absolute;
+bottom: 1%;
+width: 64px;
+height: 64px;
+border: transparent;
+border-radius: 50%;
+background-color: rgba(255,255,255, 0.75);
+font-size: 20px;
+cursor: pointer;
+`;
